849a40bda9070e4397a3b92eaeb283db19c2ec24
Gocc <Gocc@PC>
2020-05-15 15:16:00 +0200

    Add numbering on result.csv file

--- BeniFFTCucc/Program.cs
+++ BeniFFTCucc/Program.cs
@@ -13,32 +13,22 @@ namespace BeniFFTCucc
 
             List<DataModel> datas = new DataParser("asd.csv").LoadFromTextFile();
 
+
+            using var sw = new StreamWriter("result.csv");
+            sw.WriteLine("No,Ido,Bemenet,Re,Im,Magnitude,dB,Phase");
             foreach (var d in datas)
             {
+
                 List<Complex> cmplx = new List<Complex>();
                 foreach (var item in d.RawData)
                     cmplx.Add(new Complex(item));
 
-                var result = new FourierTransformation().FastFourierTransformation(cmplx.Take(128).ToArray());
-
-
-                foreach (var item in result)
+                var result = new FourierTransformation().FastFourierTransformation(cmplx.Take(256).ToArray());
+                for (int i = 0; i < result.Length; i++)
                 {
-                    Console.WriteLine(item.Real + "; " + item.Imaginary + " -- Mag: " + item.Magnitude + " -- Phase: " + item.Phase);
+                    sw.WriteLine($"{i},{d.RecordTime},{d.RawData[i]},{result[i].Real},{result[i].Imaginary},{result[i].Magnitude},{result[i].Decibell},{result[i].Phase}");
                 }
-                Console.WriteLine("------------");
-                Console.ReadLine();
-                Console.Clear();
             }
-
-
-            //var result = new FourierTransformation().FastFourierTransformation(data.ToArray());
-
-            //foreach (var item in result)
-            //{
-            //    Console.WriteLine(item.Real + "; " + item.Imaginary + " -- Mag: " + item.Magnitude + " -- Phase: " + item.Phase);
-            //}
-
 
         }
 

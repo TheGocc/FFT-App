b47c59b83e0682eca5670aafffcd46548e8b06d6
Gocc <Gocc@PC>
2020-05-19 10:52:25 +0200

    test github extension

--- BeniFFTCucc/Program.cs
+++ BeniFFTCucc/Program.cs
@@ -13,7 +13,7 @@ namespace BeniFFTCucc
 
             List<DataModel> datas = new DataParser("asd.csv").LoadFromTextFile();
 
-
+            //asd
             using var sw = new StreamWriter("result.csv");
             sw.WriteLine("No,Ido,Bemenet,Re,Im,Magnitude,dB,Phase");
             foreach (var d in datas)
